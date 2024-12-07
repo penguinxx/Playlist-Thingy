9cfc9894554c88213ea0a34f8906efe26cdb5777
CillyDev <abdulaziz.nabaa@gmail.com>
2024-12-08 00:18:16 +0100

    latest buttons added

---
+++ tictactoe/src/components/StandardButton.tsx
@@ -0,0 +1,33 @@
+interface IButton
+{
+    class_name: string
+    link: string
+    image?: string
+    imgClass?:string
+    text?: string
+    textClass?:string
+}
+
+export function StandardButton({ class_name, link, image, imgClass, text, textClass }: IButton) {
+
+
+    return (
+        <button
+            className={class_name}
+            onClick={() => {
+                window.open(link);
+            }}
+        >
+            {image && <img src={image} className={imgClass}/>}
+            {text && <span className={textClass}>{text}</span>}
+        </button>
+    );
+}
+
+
+
+
+
+  
+
+
---
+++ tictactoe/src/configured-components/standard-buttons.tsx
@@ -0,0 +1,55 @@
+import {StandardButton} from '../components/StandardButton'
+
+export const RockPlaylistButton = () =>
+(
+    <StandardButton
+    class_name="standard-button"
+    link = "https://www.youtube.com/watch?v=HhVTuowsBrQ&list=PLD1W2UDUZ6NuiYQfr830ftP5EK_7lliuE&pp=gAQB"
+    image="https://img.icons8.com/win10/512w/FFFFFF/rock-music.png"
+    imgClass="standard-button-image"
+    text = "Rock & Metal"
+    textClass="discript-style"
+    />
+    
+);
+
+export const AlternativePlaylistButton = () =>
+    (
+        <StandardButton
+        class_name="standard-button"
+        link = "https://www.youtube.com/watch?v=IOPC6JdjOwo&list=PLD1W2UDUZ6NsX24jxXfNUsh1lrlrdjGv6"
+        image="https://images.vexels.com/media/users/3/130738/isolated/preview/da484d27319aee7213579665e4257d2b-gitarren-symbol.png"
+        imgClass="standard-button-image"
+        text = "Indie & Alernative "
+        textClass="discript-style"
+        />
+        
+    );
+
+    export const WorkoutPlaylistButton = () =>
+        (
+            <StandardButton
+            class_name="standard-button"
+            link = "https://www.youtube.com/watch?v=Rr1Cdli5nE8&list=PLD1W2UDUZ6Nsif2KRwZvf8_eRuAVOldMj"
+            image="https://www.svgrepo.com/show/18057/dumbbell.svg"
+            imgClass="standard-button-image"
+            text = "Workout Playlist "
+            textClass="discript-style"
+            />
+            
+        );
+
+
+
+
+
+export const RickRollButton = () =>
+    (
+        <StandardButton
+        class_name="my-button"
+        link = "https://www.youtube.com/watch?v=HhVTuowsBrQ&list=PLD1W2UDUZ6NuiYQfr830ftP5EK_7lliuE&pp=gAQB"
+        text = "A secret lies ahead Lu ~ Click me"
+        />
+        
+    );
+
